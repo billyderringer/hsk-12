e2266385df43970673066dccf0dfd9fc4fb853d5
Billy Derringer <billyderringer7@gmail.com>
2018-04-13 00:44:06 -0500

    added get/delete subject methods

--- src/controller/subject.js
+++ src/controller/subject.js
@@ -2,6 +2,8 @@ import {Router} from 'express'
 import Student from '../model/student'
 import Subject from '../model/subject'
 import { authenticate } from '../middleware/authMiddleware'
+import Assignment from "../model/assignment";
+import SchoolTerm from "../model/schoolTerm";
 
 export default ({config, db}) => {
     let api = Router();
@@ -32,6 +34,59 @@ export default ({config, db}) => {
                     });
                 });
     });
+
+    //Get subject by id
+    api.get('/:subjectId', (req, res) => {
+        Subject.findById(req.params.subjectId, (err, subject) => {
+            console.log(subject)
+            if(subject === null){
+                res.json('subject not found')
+            }
+            else if (err) {
+                res.send(err);
+            }
+            else {
+                res.json(subject);
+            }
+        });
+    });
+
+    // Delete subject
+    api.delete('/remove/:subjectId', authenticate, (req, res) => {
+        let id = req.params.subjectId
+
+        Subject.findById(id, (err, subject) => {
+            console.log(subject)
+            if (err) {
+                res.send(err + ' :err finding subject by id')
+            }
+            Student.find({subjects: id}, (err, student) => {
+                if (err) {
+                    res.send(err + ' :err finding student by subjectId')
+                }
+                student[0].subjects.pull(subject)
+                student[0].save(err => {
+                    if (err) {
+                        res.send(err + ' :err saving subject')
+                    }
+                })
+            })
+            Subject.remove({_id: req.params.subjectId}, err => {
+                if (err) {
+                    res.send(err + ' :err removing Subject')
+                }
+                Assignment.remove({subject: id}, (err, assignment) => {
+                    if (err) {
+                        res.send(err+' :err removing assignment in subject')
+                    }
+                    if (assignment === null) {
+                        res.status(404).send("assignment not found")
+                    }
+                })
+            })
+        })
+        res.json({message: "subject successfully removed"})
+    })
 
     return api;
 }
