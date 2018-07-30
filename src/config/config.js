9067f36b36eca0328dc24704e2d16106dd53767d
Billy Derringer <billyderringer7@gmail.com>
2018-07-30 14:57:16 -0500

    changed mongoUrl

--- src/config/config.js
+++ src/config/config.js
@@ -1,10 +1,7 @@
-import authMiddleware from '../middleware/authMiddleware'
+//import authMiddleware from '../middleware/authMiddleware'
 
 export default {
     "port": "process.env.PORT || 3000",
-    "mongoUrl": "mongodb://" +
-        authMiddleware.username + ":" +
-        authMiddleware.password +
-        "@@ds031903.mlab.com:31903/heroku_t5267h8q",
+    "mongoUrl": "mongodb://<dbuser>:<dbpassword>@ds031903.mlab.com:31903/heroku_t5267h8q",
     "bodyLimit": "100kb"
 }
