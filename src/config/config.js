30d430364afac193cfd0835f5402183b029046a9
Billy Derringer <billyderringer7@gmail.com>
2018-07-29 22:08:22 -0500

    changed port to test

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 export default {
-    "port": 8000,
+    "port": "https://hsk-12.herokuapp.com",
     "mongoUrl": "mongodb://<dbuser>:<dbpassword>@ds031903.mlab.com:31903/heroku_t5267h8q",
     "bodyLimit": "100kb"
 }
