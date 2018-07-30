cf49e45cff54dd8c553545cca0e399fed5d335fa
Billy Derringer <billyderringer7@gmail.com>
2018-07-29 22:17:21 -0500

    changed monoUrl

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 export default {
     "port": "https://hsk-12.herokuapp.com",
-    "mongoUrl": "mongodb://<dbuser>:<dbpassword>@ds031903.mlab.com:31903/heroku_t5267h8q",
+    "mongoUrl": "mongodb://username:password@@ds031903.mlab.com:31903/heroku_t5267h8q",
     "bodyLimit": "100kb"
 }
