5f785bcb6720d62f6228a983945510077e0e6d4e
Billy Derringer <billyderringer7@gmail.com>
2018-08-18 18:53:14 -0500

    changed port to heroku

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 //import authMiddleware from '../middleware/authMiddleware'
 
 export default {
-    "port": "process.env.PORT || 3000",
+    "port": "https://hsk-12.herokuapp.com/",
     "mongoUrl": "mongodb://k12user:k12user@ds125912.mlab.com:25912/hsk-12",
     "bodyLimit": "100kb"
 }
