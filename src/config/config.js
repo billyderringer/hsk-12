4da365803f28136b5c3a85899028804665235896
Billy Derringer <billyderringer7@gmail.com>
2018-08-18 18:30:41 -0500

    changed mongoUrl

--- src/config/config.js
+++ src/config/config.js
@@ -2,6 +2,6 @@
 
 export default {
     "port": "process.env.PORT || 3000",
-    "mongoUrl": "mongodb://<dbuser>:<dbpassword>@ds031903.mlab.com:31903/heroku_t5267h8q",
+    "mongoUrl": "mongodb://k12user:k12user@ds125912.mlab.com:25912/hsk-12",
     "bodyLimit": "100kb"
 }
