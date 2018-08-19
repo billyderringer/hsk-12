3e76a720d42970b63e34ca55cb436f938855577e
Billy Derringer <billyderringer7@gmail.com>
2018-08-18 21:47:05 -0500

    added cors options

--- src/controller/teacher.js
+++ src/controller/teacher.js
@@ -15,7 +15,7 @@ export default () => {
         optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
     }
 
-    app.options('/register', cors())
+    app.options('/register', "Access-Control-Allow-Origin", "*")
     // '/teacher/...' - Register new account
     api.post('/register', cors(), (req, res) => {
         Teacher.register(new Teacher({
