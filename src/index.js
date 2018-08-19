9382ffb46b851aed4810a434fbec20f42f2ef9ed
Billy Derringer <billyderringer7@gmail.com>
2018-08-18 20:16:57 -0500

    changed cors settings

--- src/index.js
+++ src/index.js
@@ -13,7 +13,9 @@ app.server = http.Server(app)
 app.disable('x-powered-by')
 
 // middleware
-app.use(cors({origin: '*'}))
+//app.use(cors({origin: '*'}))
+app.use(cors({credentials: true, origin: 'https://billyderringer.github.io'}))
+app.options('https://billyderringer.github.io', cors())
 
 //parse application/json
 app.use(bodyParser.json({
