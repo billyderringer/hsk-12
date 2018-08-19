b222dc27d7b7e33c14a5f023990cd159ec780497
Billy Derringer <billyderringer7@gmail.com>
2018-08-18 22:13:13 -0500

    changed mongourl

--- dist/config/config.js
+++ dist/config/config.js
@@ -6,8 +6,8 @@ Object.defineProperty(exports, "__esModule", {
 //import authMiddleware from '../middleware/authMiddleware'
 
 exports.default = {
-    "port": "process.env.PORT || 3000",
-    "mongoUrl": "mongodb://<dbuser>:<dbpassword>@ds031903.mlab.com:31903/heroku_t5267h8q",
+    "port": "https://hsk-12.herokuapp.com/",
+    "mongoUrl": "mongodb://k12user:k12user@ds125912.mlab.com:25912/hsk-12",
     "bodyLimit": "100kb"
 };
 //# sourceMappingURL=config.js.map
