32a0d77ea9b96c3256f2a99d104b33c485bcb39b
Billy Derringer <billyderringer7@gmail.com>
2018-08-20 21:37:37 -0500

    changed cors

--- src/index.js
+++ src/index.js
@@ -14,12 +14,13 @@ app.disable('x-powered-by')
 
 // middleware
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Origin", "https://billyderringer.github.io")
     res.setHeader("Access-Control-Allow-Credentials", "true")
     res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT")
     res.setHeader("Access-Control-Allow-Headers",
         "Access-Control-Allow-Headers, " +
-        "Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
+        "Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, " +
+        "Access-Control-Request-Headers")
 })
 
 //parse application/json
