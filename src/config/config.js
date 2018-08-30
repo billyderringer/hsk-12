b6d94cc08df9657c81339c0d428eb169c54bc97b
Billy Derringer <billyderringer7@gmail.com>
2018-08-30 16:07:20 -0500

    changed port

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 //import authMiddleware from '../middleware/authMiddleware'
 
 export default {
-    "port": "https://hsk-12.herokuapp.com/",
+    "port": process.env.PORT || 8000,
     //for dev
     //"port": "8000",
     "mongoUrl": "mongodb://k12user:k12user@ds125912.mlab.com:25912/hsk-12",
