276a877e98223bc87af89cdeb0d055557835567b
Billy Derringer <billyderringer7@gmail.com>
2018-08-31 22:13:51 -0500

    added question count to assignment schema

--- src/config/config.js
+++ src/config/config.js
@@ -2,5 +2,6 @@
 export default {
     "port": process.env.PORT || 8000,
     "mongoUrl": "mongodb://k12user:k12user@ds125912.mlab.com:25912/hsk-12",
+    //"mongoUrl": "mongodb://localhost:27017/hsk-12",
     "bodyLimit": "100kb"
 }
